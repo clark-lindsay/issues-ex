1911d2cef8aa2aa546f3f25cbf43aabb1689d8f7
clark-lindsay <clarklindsay96@gmail.com>
2021-03-07 11:19:37 -0500

    Change function name for clarity

--- lib/issues/cli.ex
+++ lib/issues/cli.ex
@@ -8,12 +8,12 @@ defmodule Issues.CLI do
   def parse_args(argv) do
     OptionParser.parse(argv, switches: [help: :boolean], aliases: [h: :help])
       |> elem(1)
-      |> to_internal_representation()
+      |> args_to_internal_representation()
   end
 
-  defp to_internal_representation([ user, project, count ]) do
+  defp args_to_internal_representation([ user, project, count ]) do
     { user, project, String.to_integer(count) } 
   end
-  defp to_internal_representation([user, project]), do: { user, project, @default_count }
-  defp to_internal_representation(_), do: :help
+  defp args_to_internal_representation([user, project]), do: { user, project, @default_count }
+  defp args_to_internal_representation(_), do: :help
 end
